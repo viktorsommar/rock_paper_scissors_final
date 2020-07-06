199a9a221c21ea788d083d6a2edb7bda5ccad294
viktor_sommar <vsommarbaillot@gmail.com>
2020-07-06 20:41:05 +0200

    Adding buttons for rock paper scissors.

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,23 @@ import Player from "./Player";
 const symbols = ["rock", "paper", "scissors"];
 
 class App extends Component {
+  state = {
+    playerOne: symbols[0],
+    playerTwo: symbols[0],
+    winner: ""
+  }
+
   render() {
     return(
       <>
       <h1>Play a game of Rock Paper Scissors</h1>
       <div>
         <Player />
+      </div>
+      <div>
+        <button>Rock</button>
+        <button>Paper</button>
+        <button>Scissors</button>
         <button>Play!</button>
       </div>
       </>
--- src/Player.jsx
+++ src/Player.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Player = (props) => {
+
   return(
     <div className="ui container">
       Player 1

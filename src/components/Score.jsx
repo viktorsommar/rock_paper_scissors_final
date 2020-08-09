97df59d7208a1331f6a666e0c98ca006fd787f3b
viktor_sommar <vsommarbaillot@gmail.com>
2020-08-09 18:20:06 +0200

    Adding some styling to the scoreboard

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+import { Segment, Divider, Grid, Header, Icon } from 'semantic-ui-react'
 
 const Score = (props) => {
   return(
-    <h4 id="scoreboard">
-      <span>Player Score {props.playerScore}</span>
-      <span> - </span>
-      <span>{props.computerScore} Computer Score</span>
-    </h4>
+    <Segment placeholder id="scoreboard">
+      <Grid columns={2} stackable textAlign='center'>
+        <Divider vertical>VS</Divider>
+
+        <Grid.Row verticalAlign='middle'>
+          <Grid.Column>
+            <Header icon>
+              <Icon name='user secret' />
+              Player Score {props.playerScore}
+            </Header>
+          </Grid.Column>
+
+          <Grid.Column>
+            <Header icon>
+              <Icon name='android' />
+              {props.computerScore} Computer Score
+            </Header>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
   )
 }
 
